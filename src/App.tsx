aad1166837426657df0c251a465c81d752d69ca5
ÑPaul Sorkin <sorkinpavel@mail.ru>
2023-08-08 22:16:12 +0300

    feat: add post edditing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Admin, Resource } from "react-admin";
 import {dataProvider} from "./dataProvider";
-import { PostList } from "./posts";
+import { PostList, PostEdit } from "./posts";
 import { UserList } from "./users";
 
 export const App = () => <Admin dataProvider={dataProvider}>
-  <Resource name="posts" list={PostList} />
+  <Resource name="posts" list={PostList} edit={PostEdit} />
   <Resource name="users" list={UserList} recordRepresentation="name" />
 </Admin>;
