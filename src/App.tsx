25c68eda07bee26266e6c9c2bee082d01ba5f805
ÑPaul Sorkin <sorkinpavel@mail.ru>
2023-08-08 22:29:23 +0300

    feat: Add Menu Icons from MUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import { Admin, Resource } from "react-admin";
 import {dataProvider} from "./dataProvider";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { UserList } from "./users";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
 
-export const App = () => <Admin dataProvider={dataProvider}>
-  <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-  <Resource name="users" list={UserList} recordRepresentation="name" />
-</Admin>;
+export const App = () => (
+    <Admin dataProvider={dataProvider}>
+      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+      <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name" />
+    </Admin>
+);
